a0b5a06abfc27948b4160da22f2d6b4f982b431e
bshintak <beatriz.shintaku@gmail.com>
2023-05-28 21:40:46 +0100

    feat: ex05 cpp01

---
+++ 01/ex05/Harl.cpp
@@ -0,0 +1,31 @@
+#include "Harl.hpp"
+
+Harl::Harl() {}
+
+Harl::~Harl() {}
+
+void Harl::debug(void) { 
+	std::cout << "Debug: " << DEBUG << std::endl; 
+}
+void Harl::info(void) {
+	std::cout << "Info: " << INFO << std::endl; 
+}
+void Harl::warning(void) { 
+	std::cout << "Warning: " << WARNING << std::endl;
+}
+void Harl::error(void) {
+	std::cout << "Error: " << ERROR << std::endl;
+}
+
+void Harl::complain(std::string level) {
+	// array de tamanho 4, onde cada elemento é um ponteiro para uma função membro da classe Harl
+    void (Harl::*options[4])() = {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
+    std::string option_type[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+
+    for (int i = 0; i < 4; i++) {
+        if (level == option_type[i])
+			return (this->*options[i])();
+		// o uso de this garante que a função membro seja chamada no objeto atual da classe Harl
+    }
+    std::cerr << INVALID << std::endl;
+}
