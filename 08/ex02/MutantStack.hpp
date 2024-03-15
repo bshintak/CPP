8b5dc79a34f372b2c3f438b9afeab8e2c7e1f566
bshintak <beatriz.shintaku@gmail.com>
2024-03-15 17:46:16 +0000

    cpp08 - ex02

--- 08/ex02/MutantStack.hpp
+++ 08/ex02/MutantStack.hpp
@@ -5,8 +5,30 @@
 
 template<typename T>
 class MutantStack : public std::stack<T> {
-  MutantStack();
-  MutantStack(const MutantStack& src);
-  ~MutantStack();
+  public :
+		typedef typename std::stack<T>::container_type::iterator iterator;
+		typedef typename std::stack<T>::container_type::const_iterator const_iterator;
+
+    MutantStack() {};
+    ~MutantStack() {};
+    MutantStack(const MutantStack& src) : std::stack<T>(src) {};
+    MutantStack &operator=(const MutantStack &op) {
+	    if (this != &op)
+		    std::stack<T>::operator=(op);
+	    return (*this);
+    };
+
+    iterator begin() {
+      return (this->c.begin());
+    };
+    iterator end() {
+      return (this->c.end());
+    };
+    const_iterator begin() const {
+      return (this->c.begin());
+    };
+    const_iterator end() const {
+      return (this->c.begin());
+    };
 
 };
--- 08/ex02/main.cpp
+++ 08/ex02/main.cpp
@@ -2,6 +2,7 @@
 
 int main()
 {
+  std::cout << "Subject test:" << std::endl;
   MutantStack<int> mstack;
   mstack.push(5);
   mstack.push(17);
@@ -17,11 +18,11 @@ int main()
   MutantStack<int>::iterator ite = mstack.end();
   ++it;
   --it;
-  while (it != ite)
-  {
-  std::cout << *it << std::endl;
-  ++it;
+  while (it != ite) {
+    std::cout << *it << std::endl;
+    ++it;
   }
   std::stack<int> s(mstack);
+
   return 0;
 }
